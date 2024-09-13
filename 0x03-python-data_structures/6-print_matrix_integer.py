7a718b378973833ddb4d50804e083a597db8f98c
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-09-13 02:05:02 -0700

    add 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for list in matrix:
+            for element in list:
+                print("{:d}".format(element),
+                      end=" " if element != list[-1] else "")
+            print()
