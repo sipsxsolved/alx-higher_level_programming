60163cca2ed418fa472f3be0dfd3801517f067a0
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-09-18 00:03:55 -0700

    add 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Desc:
+    This function returns the number of keys in a dict
+
+Args:
+    a_dict (dict): The dictionary to be processed
+
+Returns:
+    int: number of keys
+"""
+
+
+def number_keys(a_dictionary):
+    return len(a_dictionary.keys())
