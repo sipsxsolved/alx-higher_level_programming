9800b2be231426ff84804f2d2912732528b80076
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-09-18 00:02:47 -0700

    add 4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Desc:
+    This function does something.
+
+Args:
+    arg1 (int): Description of the first argument.
+    arg2 (str): Description of the second argument.
+
+Returns:
+    bool: Describe the return value, if any.
+"""
+
+
+def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
