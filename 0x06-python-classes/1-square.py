9aed2b6556f500d6c92eca70d47ab1bc8dee111a
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-10-01 00:34:13 -0700

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a Square class"""
+
+
+class Square:
+    """A square class"""
+    def __init__(self, size):
+        """
+        Initialize a square with size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
