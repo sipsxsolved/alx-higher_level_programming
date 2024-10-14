49264a1e44df710dbedea3fd042f63de17981b0a
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-10-14 00:10:27 -0700

    add 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """This class will the the blueprint for its instances"""
+    def area(self):
+        """A function yet to be implemented"""
+        raise Exception("area() is not implemented")
