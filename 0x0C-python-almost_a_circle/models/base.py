e4ba8bce81ebe51097075a268a15ef9bffa9c8b2
sipsxsolved <siphesihle.xulu25@gmail.com>
2024-10-21 00:54:32 -0700

    add base.py and tests/

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module contains a Base class"""
+import json
+
+
+class Base:
+    """This is a Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """The JSON string repr of a list of dicts"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+
+        Args:
+            cls: the class itself i.e the class
+            that calls the method (since this is a classmethod)
+            list_objs: list of objects
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(Base.to_json_string(list_dicts))
